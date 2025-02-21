77a4de0d0bfa9e7d46d02e6dccfedeb58c867ecf
czfadmin <abchen9527@outlook.com>
2025-02-21 12:30:58 +0800

    chore(/): 提交代码

--- lua/readonly.lua
+++ lua/readonly.lua
@@ -19,6 +19,18 @@ M.config = config
 M.setup = function(args)
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
+function M.setup(opts)
+  -- 合并用户配置
+  M.options = vim.tbl_deep_extend("force", M.options, opts or {})
+
+  local group = vim.api.nvim_create_augroup("ReadOnlyBuffers", {
+    clear = true
+  })
+  vim.api.nvim_create_autocmd("BufEnter", {
+    group = group,
+    callback = set_read_only
+  })
+end
 
 M.hello = function()
   return module.my_first_function(M.config.opt)
